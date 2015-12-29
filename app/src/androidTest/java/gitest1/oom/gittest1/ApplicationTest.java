0cc43fde0f28356ff7ca99ed63e75d155378c2f3
superliqiang <superliqiang@126.com>
2015-12-29 23:02:32 +0800

    Initial commit

---
+++ app/src/androidTest/java/gitest1/oom/gittest1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gitest1.oom.gittest1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
